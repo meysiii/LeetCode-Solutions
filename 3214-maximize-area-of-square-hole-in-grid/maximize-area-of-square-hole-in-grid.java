b531c6556f605231510f56cec16cd6186685bcb1
Meysintha Suresh <meysintha2006@gmail.com>
2026-06-01 20:24:34 +0530

    Time: 5 ms (61.34%) | Memory: 45.2 MB (90.72%) - LeetSync

---
+++ 3214-maximize-area-of-square-hole-in-grid/maximize-area-of-square-hole-in-grid.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maximizeSquareHoleArea(int n, int m, int[] hBars, int[] vBars) {
+        int maxH = longestConsecutive(hBars);
+        int maxV = longestConsecutive(vBars);
+
+        int side = Math.min(maxH, maxV) + 1;
+
+        return side * side;
+    }
+
+    private int longestConsecutive(int[] bars) {
+        Arrays.sort(bars);
+
+        int max = 1;
+        int curr = 1;
+
+        for (int i = 1; i < bars.length; i++) {
+            if (bars[i] == bars[i - 1] + 1) {
+                curr++;
+            } else {
+                curr = 1;
+            }
+
+            max = Math.max(max, curr);
+        }
+
+        return max;
+    }
+}
